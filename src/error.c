f1587bd99a9d5da733b935c86e4e65247107af51
Alexis Coquelet <acoquele@fu-r10-p1.s19.be>
2022-03-09 10:44:48 +0100

    modified file

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: acoquele <acoquele@student@.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 11:41:58 by acoquele          #+#    #+#             */
-/*   Updated: 2022/02/28 16:55:48 by acoquele         ###   ########.fr       */
+/*   Updated: 2022/03/08 13:32:31 by acoquele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,7 @@
 
 void file_void()
 {
-	write(1,"ERROR\n",6);
-	write(1,"--> the file does not exist\n",28);
-	write(1,"<<OR>>\n", 7);
-	write(1,"--> you have misspelled the name\n",34);
-	write(1,"<<OR>>\n", 7);
-	write(1,"--> you do not have the permission to read the file\n",53);
-
+	perror("error");
 	exit(0);
 }
 
--- src/verif_funct.c
+++ src/verif_funct.c
@@ -6,7 +6,7 @@
 /*   By: acoquele <acoquele@student@.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 10:32:56 by acoquele          #+#    #+#             */
-/*   Updated: 2022/02/28 16:56:03 by acoquele         ###   ########.fr       */
+/*   Updated: 2022/03/08 13:33:17 by acoquele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int main(int argc, char **argv)
 		verif.verif = ft_strrchr(argv[1], '.');
 		if (!verif.verif)
 			error_name();
-		printf("%s\n",verif.verif);
 		verif.vrf_lenght = ft_strlen(verif.verif);
 		if (verif.vrf_lenght != 4)
 			error_name();
